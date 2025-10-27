9491b20c3459a51219259620a7e3d1029bc0d05e
Joel Kram <joelnskram@gmail.com>
2025-10-27 20:50:10 +0000

    refactoring project

--- @latest/world/World.js
+++ @latest/world/World.js
@@ -2,22 +2,12 @@ import * as THREE from 'three';
 export class World {
   constructor(canvas) {
     this.canvas = canvas;
+
     this.sizes = {
       height: canvas.height,
       width: canvas.width,
     };
     this.scene;
   }
-  init() {
-    this.renderer = new THREE.WebGLRenderer({ canvas });
-  }
-
-  getSizes() {
-    console.log(this.sizes);
-    console.log(this.renderer);
-  }
-
-  createScene() {
-    this.scene = new THREE.scene();
-  }
+  
 }
