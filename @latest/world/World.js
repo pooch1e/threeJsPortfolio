4edf371d1cd1709b6f01d85370c99dd6359fe33d
Joel Kram <joelnskram@gmail.com>
2025-10-28 17:08:33 +0000

    add event emitter class from threejsjourney for event listening utils

--- @latest/world/World.js
+++ @latest/world/World.js
@@ -1,10 +1,16 @@
 import * as THREE from 'three';
-import { Sizes } from './utils/SIzes';
-
+import { Sizes } from './utils/Sizes.js';
 
 export class World {
   constructor(canvas) {
     this.canvas = canvas;
     this.sizes = new Sizes();
+
+    // Resize event
+    this.sizes.on('resize', () => {
+      console.log('A resize occurred');
+    });
+
+    this.scene = new THREE.Scene();
   }
 }
--- @latest/world/utils/Sizes.js
+++ @latest/world/utils/Sizes.js
@@ -1,18 +1,24 @@
+import EventEmitter from './EventEmitter';
+
 // Assuming window size always fills canvas
-export class Sizes {
+export class Sizes extends EventEmitter {
   constructor() {
+    super();
     this.width = window.innerWidth;
     this.height = window.innerHeight;
     this.aspect = () => {
       return this.width / this.height;
     };
-    this.pixelRation = Math.min((window.devicePixelRatio, 2));
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
     // Resize event
     window.addEventListener('resize', () => {
       this.width = window.innerWidth;
       this.height = window.innerHeight;
       this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+      this.trigger('resize');
     });
+
+    this.trigger('resize');
   }
 }
