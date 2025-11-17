1792da1111e0af9dbbaf5deec5710252859872b5
Joel Kram <joelnskram@gmail.com>
2025-11-17 16:40:33 +0000

    add angles to galaxy xz animation

--- world/shaderTestWorld/shaders/galaxy/vertex.glsl
+++ world/shaderTestWorld/shaders/galaxy/vertex.glsl
@@ -9,6 +9,16 @@ void main() {
      * Position
      */
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // animating with time
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+    
+    angle += angleOffset;
+    modelPosition.x = cos(angle);
+    modelPosition.z = sin(angle);
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
