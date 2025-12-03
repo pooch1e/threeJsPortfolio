46e2060ca61358e34587911129933f255ad2df32
Joel Kram <joelnskram@gmail.com>
2025-12-03 22:20:21 +0000

    add: better color for smoke

--- world/shaderTestWorld/shaders/coffeeSmoke/fragment.glsl
+++ world/shaderTestWorld/shaders/coffeeSmoke/fragment.glsl
@@ -18,18 +18,20 @@ void main() {
   smoke = smoothstep(0.4, 1.0, smoke);
 
   // Edges
-  // Test tone
-  // smoke = 1.0;
-  // left edge
+  /*
+    Test tone
+    smoke = 1.0;
+  */  
+  // Left edge
   smoke *= smoothstep(0.0, 0.1, vUv.x);
-  // right edge
+  // Right edge
   smoke *= smoothstep(1.0, 0.9, vUv.x);
   // Top Edge
   smoke *= smoothstep(0.0, 0.1, vUv.y);
   // Bottom Edge
   smoke *= smoothstep(1.0, 0.9, vUv.y);
 
-  gl_FragColor = vec4(1, 1, 1, smoke);
+  gl_FragColor = vec4(0.6, 0.3, 0.2, smoke);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
