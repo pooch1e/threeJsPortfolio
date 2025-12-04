377eb0cc3337a102c42aaae521ef9cea3d2234ec
Joel Kram <joelnskram@gmail.com>
2025-12-04 02:21:29 +0000

    add: fresnel effect to holograph frag shader;

--- world/shaderTestWorld/shaders/holographic/fragment.glsl
+++ world/shaderTestWorld/shaders/holographic/fragment.glsl
@@ -1,16 +1,25 @@
 varying vec3 vPosition;
 uniform float uTime;
+varying vec3 vNormal;
 void main()
 {
+
+  // Normal
+    vec3 normal = normalize(vNormal);
 
     // Stripes pattern
     float stripes = mod((vPosition.y - uTime * 0.2) * 20.0, 1.0);
     stripes = pow(stripes, 3.0);
 
+     // Fresnel
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    float fresnel = dot(viewDirection, normal) + 1.0;
+    fresnel = pow(fresnel, 2.0);
+
     
   
     // Final color
-    gl_FragColor = vec4(1.0, 1.0, 1.0, stripes);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, fresnel);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
--- world/shaderTestWorld/shaders/holographic/vertex.glsl
+++ world/shaderTestWorld/shaders/holographic/vertex.glsl
@@ -1,9 +1,14 @@
 varying vec3 vPosition;
+varying vec3 vNormal;
 void main() {
 
-    
 
-// Position
+     // Model normal
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
+    vNormal = modelNormal.xyz;
+
+    // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     // Final position
