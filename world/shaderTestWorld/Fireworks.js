a9b28c12cf9a69064133c133b75f00a43aca3780
Joel Kram <joelnskram@gmail.com>
2025-12-09 03:02:39 +0000

    add: firework setup for geometry array

--- world/shaderTestWorld/Fireworks.js
+++ world/shaderTestWorld/Fireworks.js
@@ -6,8 +6,32 @@ export class Fireworks {
     this.scene = this.world.scene;
     this.debug = this.world.shaderExperience.debug;
 
+    // Config
+    this.parameters = {
+      count: 100,
+    };
+
     // Setup
+    this.createFirework(this.parameters.count);
     this.setDebug();
+  }
+
+  createFirework(count) {
+    const positions = new Float32Array(count * 3);
+
+    for (let i = 0; i < count; i++) {
+      const i3 = i * 3;
+
+      positions[i3] = Math.random() - 0.5;
+      positions[i3 + 1] = Math.random() - 0.5;
+      positions[i3 + 2] = Math.random() - 0.5;
+    }
+
+    this.bufferGeometry = new THREE.BufferGeometry();
+    this.bufferGeometry.setAttribute(
+      'position',
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
   }
 
   setDebug() {
