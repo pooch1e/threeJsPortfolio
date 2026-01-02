4baa72991a2277e85aaf0f81546999ec5fe3e805
Joel Kram <joelnskram@gmail.com>
2026-01-02 03:20:03 +0000

    add position to array

--- world/shaderTestWorld/GppuFlowField.js
+++ world/shaderTestWorld/GppuFlowField.js
@@ -48,6 +48,12 @@ export default class GppuFlowField {
     for (let i = 0; i < this.gpgpu.baseGeometry.count; i++) {
       const i3 = i * 3;
       const i4 = i * 4;
+
+      // Position based on Geometry
+      this.gpgpu.baseParticleTexture.image.data[i4 + 0] = this.baseGeometry.instance.attributes.position.array[i3 + 0];
+      this.gpgpu.baseParticleTexture.image.data[i4 + 1] = this.baseGeometry.instance.attributes.position.array[i3 + 1];
+      this.gpgpu.baseParticleTexture.image.data[i4 + 2] = this.baseGeometry.instance.attributes.position.array[i3 + 2];
+      this.gpgpu.baseParticleTexture.image.data[i4 + 3] = this.baseGeometry.instance.attributes.position.array[i3 + 3];
     }
 
     // Particle Variable
--- world/shaderTestWorld/shaders/gppuParticle/particles.glsl
+++ world/shaderTestWorld/shaders/gppuParticle/particles.glsl
@@ -1,3 +1,4 @@
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  vec2 uv = gl_FragCoord.xy / resolution.xy;
+  gl_FragColor = vec4(uv, 0.0, 1.0);
 }
