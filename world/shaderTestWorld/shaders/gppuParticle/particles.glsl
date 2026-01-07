9ade9cae45b11e0a5b50f059260881b0a3c3e55d
Joel Kram <joelnskram@gmail.com>
2026-01-07 11:19:40 +0000

    add simplex noise to particle position

--- world/shaderTestWorld/shaders/gppuParticle/particles.glsl
+++ world/shaderTestWorld/shaders/gppuParticle/particles.glsl
@@ -1,9 +1,16 @@
 
+#include ../includes/simplexNoise4d.glsl
 
 void main() {
   vec2 uv = gl_FragCoord.xy / resolution.xy;
   vec4 particle = texture2D(uParticles, uv);
   
-  
+  // Flow field
+  vec3 flowField = vec3(
+    simplexNoise4d(vec4(particle.xyz, 0.0)), simplexNoise4d(vec4(particle.xyz, 1.0)), simplexNoise4d(vec4(particle.xyz, 2.0))
+  );
+  flowField = normalize(flowField);
+  particle.xyz += flowField;
+
   gl_FragColor = particle;
 }
