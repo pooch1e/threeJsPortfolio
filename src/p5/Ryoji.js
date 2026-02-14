c38d4c8ce3a843f4c553fa44d3ea161e441c6ebc
Joel Kram <joelnskram@gmail.com>
2026-02-14 11:02:31 +0000

    changed random pos x,y

--- src/p5/Ryoji.js
+++ src/p5/Ryoji.js
@@ -7,7 +7,7 @@ export class Ryoji {
     this.height = height;
     this.x = 0;
     this.y = 0;
-    this.randomPos = new Vector2(p.random(0, 255), p.random(0, 255));
+    this.randomPos = new Vector2(p.random(0, this.width/2), p.random(0, this.height/2));
   }
 
   setup(width, height) {
