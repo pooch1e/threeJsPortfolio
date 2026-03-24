674c3053a2470e18d667616ebd9c9f9428f4da58
Joel Kram <joel.kram@believe.com>
2026-03-24 10:39:30 +0000

    replace gin with chi

---
+++ server/cmd/api.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+	// logger
+	// mount
+	// run
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	return r
+}
+
+type config struct {
+	adr string
+	db  dbConfig
+}
+
+type dbConfig struct {
+}
