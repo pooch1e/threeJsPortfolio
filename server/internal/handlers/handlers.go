30a7293b53d28f9608be2ff20485a1a5902f7dc1
Joel Kram <joel.kram@believe.com>
2026-03-25 19:08:18 +0000

    add json util func

--- server/internal/handlers/handlers.go
+++ server/internal/handlers/handlers.go
@@ -1,15 +1,12 @@
 package handlers
 
-import "log"
-
 // take request - do something with it/ sanatise whatever
 // then pass to repo (for db)
 
-func SanitizeQueryParam(queryParam string) map[string]any {
-	log.Printf("query param is %s", queryParam)
-	var status bool = false
-	if queryParam != "" {
-		status = true
-	}
-	return map[string]any{queryParam: "logged In", "status": status}
+type Params struct {
+	username string
+}
+
+func GetUser(params *Params) bool {
+	
 }
