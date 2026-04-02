0abd17e650c30952c0908c236987fad17f8510b5
Joel Kram <joel.kram@believe.com>
2026-04-02 16:57:44 +0100

    update form with proper controls;

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -2,6 +2,8 @@ import { useForm } from "react-hook-form";
 export default function SignUpPage() {
   const {
     control,
+    handleSubmit,
+    register,
     formState: { errors },
   } = useForm();
 
@@ -9,19 +11,24 @@ export default function SignUpPage() {
     // page
     <div className="grid grid-rows-1 gap-8 h-dvh items-center justify-center bg-[var(--color-bg)] font-karrik">
       {/* form */}
-      <div className="flex flex-col w-96 px-8 py-10 justify-center items-center gap-8 border rounded-md">
-        <h1>Sign Up Here</h1>
-        <form action="">
-          <div className="flex flex-col justify-center p-2 gap-2">
-            <label htmlFor="">Username</label>
-            <input type="text" className="rounded-sm" />
-            <label htmlFor="">Email</label>
-            <input type="text" className="rounded-sm" />
+      <div className="flex flex-col w-96 px-8 py-10 justify-center items-center gap-8 border border-gray-600 border-t-slate-200 rounded-md text-cyan-300">
+        <h1 className="justify-center">Sign Up Here</h1>
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col justify-center p-2">
+            <label htmlFor="username">Username</label>
+            <input type="text" className="rounded-sm" {...register("username")} />
+            <label htmlFor="email">Email</label>
+            <input type="email" {...register("email")} className="rounded-sm" />
             <div className="flex flex-col mt-4">
-            <label htmlFor="">Password</label>
-            <input type="text" className="rounded-sm" />
-            <label htmlFor="">Repeat Password</label>
-            <input type="text" className="rounded-sm" />
+              <label htmlFor="password">Password</label>
+              <input type="password" {...register("password")} className="rounded-sm" />
+              <label htmlFor="password">Repeat Password</label>
+              <input type="password" {...register("password")}className="rounded-sm" />
+              <div className="flex flex-col mt-2">
+                <button className="border rounded-sm hover:bg-gray-100 hover:translate-5">
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </form>
