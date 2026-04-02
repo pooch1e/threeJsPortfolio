96f8fe1c4d7d4a28a93b72e2f5d831dba2b85040
Joel Kram <joel.kram@believe.com>
2026-04-02 15:33:44 +0100

    add sign up form stub

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,21 +1,31 @@
+import { useForm } from "react-hook-form";
 export default function SignUpPage() {
-
-
-
+  const {
+    control,
+    formState: { errors },
+  } = useForm();
 
   return (
     // page
-    <div className="grid grid-rows-1 gap-8 h-dvh items-center justify-center bg-[var(--color-bg)] ">
+    <div className="grid grid-rows-1 gap-8 h-dvh items-center justify-center bg-[var(--color-bg)] font-karrik">
       {/* form */}
       <div className="flex flex-col w-96 px-8 py-10 justify-center items-center gap-8 border rounded-md">
         <h1>Sign Up Here</h1>
-        <div>
-        <Form>
-
-        </Form>
-        </div>
+        <form action="">
+          <div className="flex flex-col justify-center p-2 gap-2">
+            <label htmlFor="">Username</label>
+            <input type="text" className="rounded-sm" />
+            <label htmlFor="">Email</label>
+            <input type="text" className="rounded-sm" />
+            <div className="flex flex-col mt-4">
+            <label htmlFor="">Password</label>
+            <input type="text" className="rounded-sm" />
+            <label htmlFor="">Repeat Password</label>
+            <input type="text" className="rounded-sm" />
+            </div>
+          </div>
+        </form>
       </div>
-
     </div>
-  )
+  );
 }
