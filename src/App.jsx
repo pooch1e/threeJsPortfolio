d432ad31ebefbead74bfde6dfc2db5be6f381ac8
Joel Kram <joel.kram@believe.com>
2026-04-11 11:20:09 +0100

    add basic firefly geometry

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import './App.css';
+import "./App.css";
 
+import HomePage from "./pages/HomePage";
+import AnimalRenderPage from "./pages/AnimalRenderPage";
+import PointCloudPage from "./pages/PointCloudPage";
+import ShaderPage from "./pages/ShaderPage";
+import PortalPage from "./pages/PortalPage";
+import { Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import AnimalRenderPage from './pages/AnimalRenderPage';
-import PointCloudPage from './pages/PointCloudPage';
-import ShaderPage from './pages/ShaderPage';
-import PortalPage from './pages/PortalPage'
-import { Routes, Route } from 'react-router-dom';
-
-import SineWavePage from './pages/SineWavePage';
-import SignUpPage from './pages/SignUpPage';
-import MainLayout from './layout/MainLayout';
-
-
+import SineWavePage from "./pages/SineWavePage";
+import SignUpPage from "./pages/SignUpPage";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<MainLayout />}/>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/pointCloud" element={<PointCloudPage />}></Route>
-        <Route path="/animalPage" element={<AnimalRenderPage />}></Route>
-        <Route path="/shaders" element={<ShaderPage />}></Route>
-        <Route path="/sineWave" element={<SineWavePage />}></Route>
-        <Route path="/portalblend" element={<PortalPage />}></Route>
-
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/pointCloud" element={<PointCloudPage />}></Route>
+          <Route path="/animalPage" element={<AnimalRenderPage />}></Route>
+          <Route path="/shaders" element={<ShaderPage />}></Route>
+          <Route path="/sineWave" element={<SineWavePage />}></Route>
+          <Route path="/portalblend" element={<PortalPage />}></Route>
+        </Route>
         {/* No header/ footer */}
-        <Route path='/signup' element={<SignUpPage/>}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
       </Routes>
     </>
   );
--- world/portalWorld/Portal.js
+++ world/portalWorld/Portal.js
@@ -1,30 +1,47 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 export class Portal {
   constructor(world) {
     this.world = world;
 
-    this.scene = world.scene
-    this.debug = this.world.portalExperience.debug
+    this.scene = world.scene;
+    this.debug = this.world.portalExperience.debug;
     this.resources = world.resources;
-    this.environment = this.world.environment
+    this.environment = this.world.environment;
+    this.debug = this.world.portalExperience.debug;
+    this.renderer = this.world.portalExperience.renderer.renderer;
+    console.log(this.renderer);
 
+    this.portalModel = this.resources.items.portalModel;
+    this.portalMap = this.resources.items.portalMap;
 
+    this.debugObject = {
+      clearColor: "#43351e",
+      fireflyCount: 30,
+      fireflySize: 0.1,
+    };
 
-    this.portalModel = this.resources.items.portalModel
-    this.portalMap = this.resources.items.portalMap
+    this.renderer.setClearColor(this.debugObject.clearColor);
 
-
-    this.addLights()
-    this.setModel()
+    this.addLights();
+    this.setModel();
+    this.setFlies();
+    this.setDebug();
   }
 
-
+  setDebug() {
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder("Portal Blend");
+      this.debugFolder.addColor(this.debugObject, "clearColor").onChange(() => {
+        this.renderer.setClearColor(this.debugObject.clearColor);
+      });
+    }
+  }
 
   addLights() {
-    this.pointLight = new THREE.RectAreaLight('white', 5)
-    this.pointLight.position.y = 10
-    this.pointLight.lookAt(0, 0, 0)
-    this.scene.add(this.pointLight)
+    this.pointLight = new THREE.RectAreaLight("white", 5);
+    this.pointLight.position.y = 10;
+    this.pointLight.lookAt(0, 0, 0);
+    this.scene.add(this.pointLight);
   }
 
   setModel() {
@@ -36,9 +53,24 @@ export class Portal {
     //   }
     // });
     // texture looks SHIT - use built in for now
-    this.portalModel.scene.rotation.y = -90
+    this.portalModel.scene.rotation.y = -90;
     this.scene.add(this.portalModel.scene);
+  }
 
+  setFlies() {
+    this.geometry = new THREE.BufferGeometry();
+    const positions = new Float32Array(this.debugObject.fireflyCount * 3)
+    for (let i = 0; i < this.debugObject.fireflyCount; i++) {
+      positions[i * 3] = Math.random() * 2
+      positions[i * 3 + 1] = Math.random() * 2
+      positions[i * 3 + 2] = Math.random() * 2
+    }
+
+    this.geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+    this.firefliesMaterial = new THREE.PointsMaterial({ size: 0.1, sizeAttenuation: true })
+    this.fireflyPoints = new THREE.Points(this.geometry, this.firefliesMaterial)
+    this.scene.add(this.fireflyPoints)
   }
 
   update(time) {
