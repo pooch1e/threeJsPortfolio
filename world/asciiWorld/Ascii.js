d12e443b4d1d5de7934e3f2ff047dc1b491798f6
Joel Kram <joel.kram@believe.com>
2026-04-13 14:22:12 +0100

    init ascii shader page

--- world/asciiWorld/Ascii.js
+++ world/asciiWorld/Ascii.js
@@ -1,11 +1,14 @@
+import * as THREE from "three";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
 export class Ascii {
   constructor(world) {
     this.world = world;
     this.scene = world.scene;
     this.debug = world.asciiExperience.debug;
-    console.log(this.world)
 
-    // this.setDebug();
+    this.setDebug();
+    this.setPlaneGeometry();
   }
 
   setDebug() {
@@ -13,9 +16,21 @@ export class Ascii {
       this.debugFolder = this.debug.ui.addFolder("Ascii Experience");
     }
   }
+
+  setPlaneGeometry() {
+    this.planeGeometry = new THREE.PlaneGeometry(10, 10, 50);
+    this.planeMaterial = new THREE.ShaderMaterial({
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+    });
+    this.planeMesh = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
+    console.log(this.planeMesh);
+
+    this.scene.add(this.planeMesh);
+  }
+
   update(time) {
     if (time) {
-      
     }
   }
 
