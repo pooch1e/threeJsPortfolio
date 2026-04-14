5c5912171f526a004a28820e9c944f3be6dffdc9
Joel Kram <joel.kram@believe.com>
2026-04-14 17:26:17 +0100

    add minor styling to signup form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -12,32 +12,37 @@ export default function Login() {
   };
   return (
     <div className="grid grid-rows-1 gap-8 h-dvh items-center justify-center bg-[var(--color-bg)] font-karrik">
-      <div className="w-96 ">
-        <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          {...register("username", {
-            required: "A username is required",
-            minLength: { value: 3, message: "Min 3 characters" },
-          })}
-        />
-        {errors.username && (
-          <span className="text-red-500 text-sm">
-            {errors.username.message}
-          </span>
-        )}
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          {...register("password", { required: "A new password is required" })}
-          className="rounded-sm p-1 text-black"
-        />
-        <button>Login</button>
-      </form>
-      <button>Signup</button>
+      <div className="w-96 border-2 p-2 rounded-sm border-t-gray-300 shadow-card flex flex-col gap-4">
+        <div className="flex justify-center">
+          <h1>Login</h1>
+        </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 w-full"
+          >
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              {...register("username", {
+                required: "A username is required",
+                minLength: { value: 3, message: "Min 3 characters" },
+              })}
+            />
+            {errors.username && (
+              <span className="text-red-500 text-sm">
+                {errors.username.message}
+              </span>
+            )}
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              {...register("password", { required: "A password is required" })}
+              className="rounded-sm p-1 text-black"
+            />
+            <button>Login</button>
+          </form>
+        </div>
+        <button>Signup</button>
       </div>
-    </div>
   );
 }
