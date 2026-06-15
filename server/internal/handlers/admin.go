58224066697ba32923b3e1b98f04122ee0372952
Joel Kram <joel.kram@believe.com>
2026-06-15 20:48:28 +0100

    ADMIN-4 implent listUsers endpoint

--- server/internal/handlers/admin.go
+++ server/internal/handlers/admin.go
@@ -1,16 +1,83 @@
 package handlers
 
 import (
+	"log/slog"
+	"math"
 	"net/http"
+	"strconv"
 
 	json "threejsPortfolioServer/internal/json"
+	"threejsPortfolioServer/internal/models"
 	"threejsPortfolioServer/internal/repos"
 )
+
+type paginationMeta struct {
+	Page int`json:"page"`
+	Limit int`json:"limit"`
+	Total int`json:"total"`
+	TotalPages int`json:"total_pages"`
+}
+
+type listUsersResponse struct {
+	Users []models.User `json:"users"`
+	Pagination paginationMeta `json:"pagination"`
+}
 
 // Get all users
 func ListUsersHandler(repo repos.UserRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		json.WriteError(w, http.StatusNotImplemented, "not implemented")
+		pageString := r.URL.Query().Get("page")
+		pageInt, err := strconv.Atoi(pageString)
+		if err != nil {
+			slog.Error("Error converting page number", err)
+		}
+
+		if pageInt <= 1 {
+			pageInt = 1
+		}
+
+		limit := r.URL.Query().Get("limit")
+		limitInt, err := strconv.Atoi(limit)
+				if err != nil {
+			slog.Error("Error converting offset number", err)
+		}
+
+		if limitInt <= 1 {
+			limitInt = 20
+		}
+
+		if limitInt > 100 {
+			limitInt = 100
+		}
+
+		offset := (pageInt - 1) * limitInt
+
+		// call repo and get list of users
+		usersList, err := repo.GetAllUsers(limitInt, offset)
+		if err != nil {
+			json.WriteError(w, http.StatusInternalServerError, "Error in retrieving user list")
+		}
+
+		userCount, err := repo.GetUserCount()
+		if err != nil {
+			json.WriteError(w, http.StatusInternalServerError, "Error in retrieving user count")
+		}
+
+		totalPages := int(math.Ceil(float64(userCount)/float64(limitInt)))
+
+		meta := paginationMeta{
+			Page: pageInt,
+			Limit: limitInt,
+			Total: userCount,
+			TotalPages: totalPages,
+
+		}
+
+
+		json.WriteJson(w, http.StatusOK, listUsersResponse{
+			Users: usersList,
+			Pagination: meta,
+		})
 	}
 }
 
