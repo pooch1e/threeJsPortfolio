8954bf8edac210488f5a7e28d7b7f62ab8dec527
Joel Kram <joel.kram@believe.com>
2026-07-11 15:01:42 +0100

    add helper utilities for rect experience

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,3 +6,17 @@ export const randomInt = (min, max) =>
 export const randomFloat = (min, max) => {
   return Math.random() * (max - min + 1) + min;
 };
+
+// accepts array
+export const randomElement = (arr) => {
+  return arr[randomIndex(arr)];
+};
+
+// accepts array
+export const randomIndex = (arr) => {
+  return randomInt(0, arr.length - 1);
+};
+
+export const randomChance = (chance) => {
+  return Math.random() < chance;
+};
