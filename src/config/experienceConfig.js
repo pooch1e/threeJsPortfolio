687e00c9a6751e71f96b5b08abfb8ebb026d6d63
Joel Kram <joel.kram@believe.com>
2026-07-20 10:47:49 +0100

    reorder experience config

--- src/config/experienceConfig.js
+++ src/config/experienceConfig.js
@@ -1,13 +1,26 @@
 export const experiences = [
   {
     order: "001",
+    slug: "flower",
+    name: "Flower",
+    load: () =>
+      import("../../world/flowerWorld/FlowerExperience").then((m) => m.FlowerExperience),
+  },
+  {
+    order: "002",
+    slug: "rects",
+    name: "Ryoji",
+    load: () => import("../../world/rectPerception/RectExperience").then((m) => m.RectExperience),
+  },
+  {
+    order: "003",
     slug: "animalPage",
     name: "Models",
     load: () =>
       import("../../world/animalWorld/ModelExperience").then((m) => m.ModelExperience),
   },
   {
-    order: "002",
+    order: "004",
     slug: "pointCloud",
     name: "Points",
     canvasClassName: "bg-[#798086]",
@@ -15,42 +28,29 @@ export const experiences = [
       import("../../world/pointCloudWorld/PointExperience").then((m) => m.PointExperience),
   },
   {
-    order: "003",
+    order: "005",
     slug: "shaders",
     name: "Shaders",
     custom: "shader",
   },
   {
-    order: "004",
+    order: "006",
     slug: "sineWave",
     name: "Sines",
     load: () => import("../../world/sineWorld/SineExperience").then((m) => m.SineExperience),
   },
   {
-    order: "005",
+    order: "007",
     slug: "portalblend",
     name: "Portal",
     load: () =>
       import("../../world/portalWorld/PortalExperience").then((m) => m.PortalExperience),
   },
   {
-    order: "006",
+    order: "008",
     slug: "ascii",
     name: "Ascii",
     load: () => import("../../world/asciiWorld/AsciiExperience").then((m) => m.AsciiExperience),
-  },
-  {
-    order: "007",
-    slug: "flower",
-    name: "Flower",
-    load: () =>
-      import("../../world/flowerWorld/FlowerExperience").then((m) => m.FlowerExperience),
-  },
-  {
-    order: "008",
-    slug: "rects",
-    name: "Ryoji",
-    load: () => import("../../world/rectPerception/RectExperience").then((m) => m.RectExperience),
   },
 ];
 
